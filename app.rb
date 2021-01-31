997f2bc2030ebbd9d69409dd6ee6a9cf1da42915
Christerle Nounoute <christerlesn@gmail.com>
2021-01-31 02:29:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    Welcome to my website! This is the homepage
+  end
+  
   get '/reverse' do
     erb :reverse
   end
